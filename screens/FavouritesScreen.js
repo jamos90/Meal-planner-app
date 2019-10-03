46c681b88d06e6aeb41fe1186c750e508bd58c53
Jessica Ruiters <jamosamos90@gmail.com>
2019-10-03 09:24:25 -0400

    FavoritesScreen: meal item added, rendering dummy data for test purposes

--- screens/FavouritesScreen.js
+++ screens/FavouritesScreen.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
+import MealList from '../components/MealList';
+
+import { MEALS } from '../data/dummy-data';
 
 const FavoritesScreen = props => {
+
+    const favMeals = MEALS.filter(meal => meal.id === 'm1' || meal.id === 'm2');
+
     return(
-        <View style={styles.screen}>
-            <Text>FaVScreen workds</Text>
-        </View>
+        <MealList listData={favMeals} navigation={props.navigation}></MealList>
     );
 }
 
@@ -14,12 +17,5 @@ FavoritesScreen.navigationOptions = {
     headerTitle: 'Your Favorites'
 };
 
-const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-});
 
 export default FavoritesScreen
