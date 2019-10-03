51252acf11d8e5ae7a3bf54f4925da51f0464238
Jessica Ruiters <jamosamos90@gmail.com>
2019-10-03 09:17:40 -0400

    mealslist: listing meals functionality moved to meals list compment. Still rendering correctly in browser

--- components/MealList.js
+++ components/MealList.js
@@ -2,11 +2,30 @@ import React from 'react';
 
 import { FlatList, StyleSheet, View } from 'react-native';
 
+import MealItem from './MealItem';
+
 const MealList = props => {
+
+    const renderMealItem =  (itemData)=> {
+        return <MealItem 
+                    title={itemData.item.title} 
+                    duration={itemData.item.duration}
+                    complexity={itemData.item.complexity.toUpperCase()} 
+                    affordability={itemData.item.affordability}
+                    image={itemData.item.imageUrl}
+                    onSelectMeal={()=> {
+                        props.navigation.navigate({routeName: 'MealDetail', params: {
+                            mealId: itemData.item.id
+                        }})
+                    }}
+                />    
+        
+    }
+
     return (
         <View style={styles.list}>
             <FlatList 
-                data={displayedMeals} 
+                data={props.listData} 
                 keyExtractor={(item,index)=>{item.id}} 
                 renderItem={renderMealItem}
                 style={styles.listStyle}
