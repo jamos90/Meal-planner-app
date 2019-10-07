1642f9fc59d35d41c074d00bfdcbe429a3f7f011
Jessica Ruiters <jamosamos90@gmail.com>
2019-10-07 18:45:37 -0400

    mealDetailScreen: redux data being acessed and displayed

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { ScrollView, StyleSheet, View, Text, Image } from 'react-native';
 import { useSelector } from 'react-redux';
@@ -22,6 +22,10 @@ const MealDetailScreen = props => {
     const mealId = props.navigation.getParam('mealId');
 
     const selectedMeal = availableMeals.find(meal => meal.id === mealId);
+
+    useEffect(()=>{
+        props.navigation.setParams({mealTitle: selectedMeal.title})
+    }, [selectedMeal]);
     
     return(
         <ScrollView>
@@ -42,9 +46,10 @@ const MealDetailScreen = props => {
 
 MealDetailScreen.navigationOptions = (navigationData)=> {
     const mealId = navigationData.navigation.getParam('mealId');
-    const selectedMeal = MEALS.find(meal => meal.id === mealId);
+    const mealTitle = navigationData.navigation.getParam('mealTitle')
+    // const selectedMeal = mealId.find(meal => meal.id === mealId);
     return {
-        headerTitle: selectedMeal.title,
+        headerTitle: mealTitle,
         headerRight: 
         <HeaderButtons HeaderButtonComponent={HeaderButton}>
             <Item 
